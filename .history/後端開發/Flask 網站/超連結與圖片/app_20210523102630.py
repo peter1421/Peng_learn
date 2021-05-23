7c2008a4accff9c822c9d8cd697a56c8249d90b7
peter1421 <51041311+peter1421@users.noreply.github.com>
2021-05-23 10:44:52 +0800

    L9

---
+++ .history/後端開發/Flask 網站/超連結與圖片/app_20210523102630.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask import request,render_template
+app = Flask(
+    __name__,
+    static_folder="public",
+    static_url_path="/"
+)
+
+
+@app.route("/")
+def index():
+    return render_template("index",name="小花")
+
+
+
+
+app.run(port=3000)
---
+++ .history/後端開發/Flask 網站/超連結與圖片/app_20210523103720.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask import request,render_template
+app = Flask(
+    __name__,
+    static_folder="public",
+    static_url_path="/"
+)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/page")
+def index_p():
+    return render_template("page.html")
+
+
+app.run(port=3000)
